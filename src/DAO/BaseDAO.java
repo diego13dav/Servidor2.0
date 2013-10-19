081a4382e3e3946529751278eb5f26389cff910a
DouglasZen <douglas.zen@gmail.com>
2013-10-18 22:29:32 -0300

    retirada da classe jdbc, e inclusao da classe basaDAO faça um extend em todas as classes dao, a connection factory esta dentro da baseDAO

---
+++ src/DAO/BaseDAO.java
@@ -0,0 +1,83 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BaseDAO {
+
+	/**
+	 * Classe base para todos os dao's
+	 */
+	protected PreparedStatement pstmt;
+	protected Statement stmt;
+	protected ResultSet rs;
+	protected Connection connection;
+	
+	private String dbDriver = "com.mysql.jdbc.Driver";
+	private String dbUrl = "jdbc:mysql://localhost/pedidos2";
+	private String user = "root";
+	private String password = "";
+	
+	public Connection getConnection(){
+		System.out.println("Conectando ao banco de dados do web service...");
+		try{
+			Class.forName(dbDriver);
+			return DriverManager.getConnection(dbUrl,user,password);
+		} catch(SQLException e){
+			throw new RuntimeException(e);
+		} catch(ClassNotFoundException e1){
+			throw new RuntimeException(e1);
+		}
+	}
+	
+	public void finalize(){
+		try {
+			closeResources(connection, rs, pstmt, stmt);
+		} catch (SQLException e) {
+			System.out.print("BaseDAO:finalize " + e);
+			
+		}
+	}
+	
+	public void finalizaNaoConn(){
+		try {
+			closeResources(rs, pstmt, stmt);
+		} catch (SQLException e) {
+			System.out.print("BaseDAO:finalize " + e);
+			
+		}
+	}
+	
+	private void closeResources(Connection conn, ResultSet r, PreparedStatement ps, Statement s) throws SQLException{
+		if(r != null){
+			r.close();
+		}
+		if(ps != null){
+			ps.close();
+		}
+		if(s != null){
+			s.close();
+		}
+		if(conn != null && !conn.isClosed()){
+			conn.close();
+		}
+	}
+	
+	private void closeResources(ResultSet r, PreparedStatement ps, Statement s) throws SQLException{
+		if(r != null){
+			r.close();
+		}
+		if(ps != null){
+			ps.close();
+		}
+		if(s != null){
+			s.close();
+		}
+		
+	}
+
+}
